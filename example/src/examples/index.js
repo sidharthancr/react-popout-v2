139de0fd2c7f165eedd8c8224801b57c370ef785
C r, Sidharthan <sicr@paypal.com>
2020-05-13 22:51:22 +0530

    Example updated to support IE

--- example/src/examples/index.js
+++ example/src/examples/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react'
+import React from 'react'
 
 import Container from 'react-bootstrap/Container'
 import Jumbotron from 'react-bootstrap/Jumbotron'
@@ -12,10 +12,6 @@ import CloseProgrammatically from './CloseProgrammatically'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const Examples = () => {
-  const childRef = useRef()
-
-  const [isOpen, setOpen] = useState(false)
-
   return (
     <Container>
       <Jumbotron>
