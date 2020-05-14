3f0717497a7843741e44c373d0620e7dcd5613f6
C r, Sidharthan <sicr@paypal.com>
2020-05-14 11:00:55 +0530

    Updates

--- precache-manifest.aaeb6f0046027e32c483d79e3f4ea606.js
+++ precache-manifest.aaeb6f0046027e32c483d79e3f4ea606.js
@@ -1,31 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "97fbc022771b62d2030f8d7e396f76ec",
+    "revision": "61c33fa21a6c9ea1f78972300517354d",
     "url": "/react-popout-v2/index.html"
   },
   {
-    "revision": "40e5a22d0bb6a09aa6b1",
+    "revision": "ad2ee933ff8d2316692e",
     "url": "/react-popout-v2/static/css/2.53823a3d.chunk.css"
   },
   {
-    "revision": "804fe9542619ab4db3ac",
+    "revision": "30445845b4bd7034d852",
     "url": "/react-popout-v2/static/css/main.42cc8b22.chunk.css"
   },
   {
-    "revision": "40e5a22d0bb6a09aa6b1",
-    "url": "/react-popout-v2/static/js/2.72d67b83.chunk.js"
+    "revision": "ad2ee933ff8d2316692e",
+    "url": "/react-popout-v2/static/js/2.3490c629.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/react-popout-v2/static/js/2.72d67b83.chunk.js.LICENSE.txt"
+    "url": "/react-popout-v2/static/js/2.3490c629.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "804fe9542619ab4db3ac",
-    "url": "/react-popout-v2/static/js/main.3e52a279.chunk.js"
+    "revision": "30445845b4bd7034d852",
+    "url": "/react-popout-v2/static/js/main.ec24e9e7.chunk.js"
   },
   {
     "revision": "6fce53c7c7713ebf61712cc2929746fa",
-    "url": "/react-popout-v2/static/js/main.3e52a279.chunk.js.LICENSE.txt"
+    "url": "/react-popout-v2/static/js/main.ec24e9e7.chunk.js.LICENSE.txt"
   },
   {
     "revision": "8c17e63ae6bc68151bb5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-popout-v2/precache-manifest.435fc2dfed9c0739312f786de105c231.js"
+  "/react-popout-v2/precache-manifest.aaeb6f0046027e32c483d79e3f4ea606.js"
 );
 
 self.addEventListener('message', (event) => {
